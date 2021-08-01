272623f1a54080798161d5d99c56b9ec4157ac99
HM\TEMPTHRAV <thamilarasan.ravichandren@hm.com>
2021-08-01 11:47:21 +0530

    added typecript variable

--- src/app/webpage/webpage.component.ts
+++ src/app/webpage/webpage.component.ts
@@ -26,17 +26,17 @@ export class WebpageComponent implements OnInit {
   callTimer(): void {
     let countDownDate = new Date("Aug 10, 2021").getTime();
     // Update the count down every 1 second
-    var clearTimer = setInterval(() => {
+    let clearTimer = setInterval(() => {
       // Get todays date and time
-      var now = new Date().getTime();
+      let now = new Date().getTime();
       // Find the distance between now and the count down date
-      var distance = countDownDate - now;
+      let distance = countDownDate - now;
 
       // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       // Display the result in the element with id="demo"
       this.timer = days.toString() + 'd ' + hours.toString() + 'h ' + minutes.toString() + 'm ' + seconds.toString() + 's';
